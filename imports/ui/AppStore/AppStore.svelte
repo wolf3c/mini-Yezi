93071b95181d770ba0355b0061cb487ffc95d2fe
wolf3c <wolf3c@gmail.com>
2025-04-21 17:02:52 +0800

    Add files via upload

---
+++ imports/ui/AppStore/AppStore.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Meteor } from "meteor/meteor";
+    import { Apps } from "/imports/api/apps.js";
+    import { appsSubscribeIds } from "/imports/ui/components/Store/SubsStore.js";
+
+    $m: Meteor.subscribe("apps.getRecommend");
+
+    let apps = null;
+    $m: {
+        apps = Apps.find({ permission: "public" }).fetch();
+        if (apps?.length) {
+            appsSubscribeIds.add(apps.map((app) => app._id));
+        }
+    }
+</script>
